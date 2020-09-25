53586414d9d21ece55ac71dee75de1beda4c3d79
Floriana Ciaglia <flociaglia@u.boisestate.edu>
2020-09-24 20:53:12 -0600

    Fixed program structure

--- parallelSum.c
+++ parallelSum.c
@@ -7,6 +7,45 @@
 #define MIN 0
 #define MAX 5
 
+int* generateRands(int lower, int upper, int count){
+    int i;
+    int *num_array = (int *)malloc(sizeof(int)*count);
+
+    if(num_array == NULL){
+        printf("malloc failed");
+        exit(1);
+    }
+
+
+    for(i = 0; i < count; i++){
+       num_array[i] = (rand() % (upper-lower+1))+lower;
+
+    }
+
+    return num_array;
+}
+
+
+long serialSum(long randNums, int *numArray){
+
+    int i;
+    long sum = 0;
+
+    for(i=0; i<randNums; i++){
+        sum += numArray[i];
+    }
+
+    return sum;
+}
+
+long pToPSum(long randNums, int *numArray){
+
+    
+
+
+}
+
+
 int main(int argc, char **argv){
 
     long randNums;
@@ -15,12 +54,11 @@ int main(int argc, char **argv){
     if(argc != 2){
 
         printf("Invalid number of argument\n");
-        return 0;
+        return 1;
     }
 
     randNums = atoi(argv[1]);
     randNums *= 100000;
-    //printf("How many numbers?  %d\n", randNums);
     
     numArray = generateRands(MIN, MAX, randNums);
     /*int i;
@@ -28,14 +66,22 @@ int main(int argc, char **argv){
         printf("%d ", numArray[i]);
     }*/
 
-    // START TIMING HERE!
+    // SERIAL SUMMATION!
     clock_t begin = clock();
-    int sum = serialSum(randNums, numArray);
-    printf("The sum is %d\n", sum);
+
+    int serial = serialSum(randNums, numArray);
+
+    printf("The sum is %d\n", serial);
    
     clock_t end = clock();
     double time_spent = (double)(end - begin);
-    printf("Time spent: %f\n", time_spent);
-    return 1;
+
+    printf("Time spent in serial: %f\n", time_spent);
+
+    // POINT TO POINT SUMMATION!
+    
+
+
+    return 0;
 
 }   
--- parallelSum.h
+++ parallelSum.h
@@ -1,35 +1,9 @@
 //this is the header file
-
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-int* generateRands(int lower, int upper, int count){
-    int i;
-    int *num_array = (int *)malloc(sizeof(int)*count);
-
-    if(num_array == NULL){
-        printf("malloc failed");
-        exit(1);
-    }
+int* generateRands(int lower, int upper, int count);
+long serialSum(long randNums, int *numArray);
+long pTopSum(long randNums, int *numArray);
 
 
-    for(i = 0; i < count; i++){
-       num_array[i] = (rand() % (upper-lower+1))+lower;
 
-    }
-
-    return num_array;
-}
-
-long serialSum(long randNums, int *numArray){
-
-    int i;
-    long sum = 0;
-
-    for(i=0; i<randNums; i++){
-        sum += numArray[i];
-    }
-
-    return sum;
-}
