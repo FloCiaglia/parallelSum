450ab8b921cec79d43348ff0a09db5fff04d04a0
FloCiaglia <47931555+FloCiaglia@users.noreply.github.com>
2020-09-25 13:31:17 -0600

    Delete parallelSum.h

--- parallelSum.h
+++
@@ -1,9 +0,0 @@
-//this is the header file
-#include <stdio.h>
-
-int* generateRands(int lower, int upper, int count);
-long serialSum(long randNums, int *numArray);
-long pTopSum(long randNums, int *numArray);
-
-
-
